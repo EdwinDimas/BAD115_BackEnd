f2523572d8e3158224f5b46a004e5088ee072068
rr14059 <rr14059@ues.edu.sv>
2020-04-12 00:42:13 -0600

    first commit

---
+++ src/test/java/com/bad/planilla/backend/apirest/demo/PlanillaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bad.planilla.backend.apirest.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanillaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
