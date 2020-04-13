e76f59c73e6b7fd67deb23283f13a083bfddacd3
rr14059 <rr14059@ues.edu.sv>
2020-04-12 22:09:13 -0600

    update secondary

--- src/main/java/com/bad/planilla/backend/apirest/service/EmpleadoService.java
+++ src/main/java/com/bad/planilla/backend/apirest/service/EmpleadoService.java
@@ -2,7 +2,9 @@ package com.bad.planilla.backend.apirest.service;
 
 import com.bad.planilla.backend.entity.EmpleadoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmpleadoService extends JpaRepository<EmpleadoEntity, Long> {
 
 }
