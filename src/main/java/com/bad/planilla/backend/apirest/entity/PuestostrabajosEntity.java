5db3c9df179de1a3920aaa9a2bf9aee8bfe4f125
rr14059 <rr14059@ues.edu.sv>
2020-05-03 19:00:47 -0600

    update puestotrabajo

--- src/main/java/com/bad/planilla/backend/apirest/entity/PuestostrabajosEntity.java
+++ src/main/java/com/bad/planilla/backend/apirest/entity/PuestostrabajosEntity.java
@@ -12,7 +12,7 @@ public class PuestostrabajosEntity {
     private String descripcion;
     private Collection<SalariospuestostrabajosEntity> salarioPuestoTrabajo;
     private boolean estado;
-   // private SalariosEntity id_salario;
+    private SalariosEntity id_salario;
 
     @Id
     @Column(name = "id_puestotrabajo", nullable = false)
@@ -78,13 +78,13 @@ public class PuestostrabajosEntity {
         this.salarioPuestoTrabajo = salarioPuestoTrabajo;
     }
 
-//    @ManyToOne
-//    @JoinColumn(name = "id_salario", referencedColumnName = "id_salario")
-//    public SalariosEntity getId_salario() {
-//        return id_salario;
-//    }
-//
-//    public void setId_salario(SalariosEntity id_salario) {
-//        this.id_salario = id_salario;
-//    }
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_salario", referencedColumnName = "id_salario")
+    public SalariosEntity getId_salario() {
+        return id_salario;
+    }
+
+    public void setId_salario(SalariosEntity id_salario) {
+        this.id_salario = id_salario;
+    }
 }
