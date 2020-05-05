06eeed5850dd6d38c090bdfed00c31eb06f0dd37
EdwinDimas <edwinenriquedimas@gmail.com>
2020-05-04 23:20:49 -0600

    Cambiada url para permisos de CORS. v2

--- src/main/java/com/bad/planilla/backend/apirest/globals/Constants.java
+++ src/main/java/com/bad/planilla/backend/apirest/globals/Constants.java
@@ -1,6 +1,6 @@
 package com.bad.planilla.backend.apirest.globals;
 
 public class Constants {
-    public static final String URL_BASE="https://proudparrot.herokuapp.com/";
+    public static final String URL_BASE="https://proudparrot.herokuapp.com";
     public static final String BASE="/api/planilla/";
 }
