28037d6c6274e31b0a592d17c56b9a61b3d99827
Veronica <svcl_svcl@hotmail.com>
2020-05-12 05:51:43 -0600

    CRUD unidad organizacional

---
+++ src/main/java/com/bad/planilla/backend/apirest/services/UnidadOrganizacionalServiceImp.java
@@ -0,0 +1,39 @@
+package com.bad.planilla.backend.apirest.services;
+
+import com.bad.planilla.backend.apirest.entity.UnidadesorganizacionalesEntity;
+import com.bad.planilla.backend.apirest.repository.UnidadOrganizacionalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("unidadOrganizacionalService")
+public class UnidadOrganizacionalServiceImp implements IUnidadOrganizacionalService{
+    @Autowired
+    private UnidadOrganizacionalRepository unidadOrganizacionalRepository;
+
+    @Override
+    public List<UnidadesorganizacionalesEntity> list() {
+        return unidadOrganizacionalRepository.findAllByOrderByNombre();
+    }
+
+    @Override
+    public UnidadesorganizacionalesEntity guardar(UnidadesorganizacionalesEntity unidadOrganizacional) {
+        return null;
+    }
+
+    @Override
+    public UnidadesorganizacionalesEntity findById(int id) {
+        return null;
+    }
+
+    @Override
+    public UnidadesorganizacionalesEntity modificar(UnidadesorganizacionalesEntity unidadOrganizacional) {
+        return null;
+    }
+
+    @Override
+    public String desactivar(int id) {
+        return null;
+    }
+}
