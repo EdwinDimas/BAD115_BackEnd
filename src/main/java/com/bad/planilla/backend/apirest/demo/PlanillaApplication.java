4c11ed418718b801fb86557869ba3e045b9f258b
rr14059 <rr14059@ues.edu.sv>
2020-05-11 21:37:39 -0600

    centro costos history

--- src/main/java/com/bad/planilla/backend/apirest/demo/PlanillaApplication.java
+++ src/main/java/com/bad/planilla/backend/apirest/demo/PlanillaApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -12,6 +13,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @ComponentScan(basePackages={"com.bad.planilla.backend.apirest.restcontroller"})
 @EnableJpaRepositories("com.bad.planilla.backend.apirest.repository")
 @EntityScan("com.bad.planilla.backend.apirest.entity")
+@EnableJpaAuditing
 public class PlanillaApplication {
 
     public static void main(String[] args) {
