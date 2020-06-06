b1d261780e8833db09d8f4c8fec4ea921122c8ed
Veronica <svcl_svcl@hotmail.com>
2020-06-05 21:26:40 -0600

    coment

--- src/main/java/com/bad/planilla/backend/apirest/services/ICalendarioTrabajoService.java
+++ src/main/java/com/bad/planilla/backend/apirest/services/ICalendarioTrabajoService.java
@@ -7,10 +7,6 @@ import java.util.List;
 
 @Service
 public interface  ICalendarioTrabajoService {
-//    public List<CalendariostrabajosEntity>listar();
-//    public Optional<CalendariostrabajosEntity>listarId(int id);
-//    public int guardar(CalendariostrabajosEntity ca);
-//    public void eliminar(int id);
 
     public abstract List<CalendariostrabajosEntity> list();
     public abstract CalendariostrabajosEntity guardar(CalendariostrabajosEntity calendariotrabajo);
